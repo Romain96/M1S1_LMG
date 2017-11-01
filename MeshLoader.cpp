4bab5ed879ff227267de94b8bbe23dcb6628f5fa
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-11-01 15:47:00 +0100

    Ajout de la classe MeshLoader
    - pas encore écrite
    - TODO : écrire la fonction read

---
+++ MeshLoader.cpp
@@ -0,0 +1,16 @@
+/*
+================================================================================
+Classe          : MeshLoader
+Auteur(s)       : Romain PERRIN
+Date :          : 2017-10-31
+Description     : permet de charger une mesh en lisant un fichier .obj
+================================================================================
+*/
+
+#include "Mesh.h"
+#include "MeshLoader.h"
+
+int MeshLoader::read(const char *filename, Mesh& mesh)
+{
+	return 1;
+}
