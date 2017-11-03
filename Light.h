db5c5a57881c8e8d03f757c87fb97ce899324e83
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-11-03 14:46:04 +0100

    Ajout de la classe Light
    - classe Light contenant les informations relatives à la source de lumière (basée sur le TP3/4)

---
+++ Light.h
@@ -0,0 +1,40 @@
+#ifndef __LIGHT_H__
+#define __LIGHT_H__
+
+/*
+================================================================================
+Classe          : Light
+Auteur(s)       : Romain PERRIN
+Date :          : 2017-11-03
+Description     : contient les informations sur une source de lumière (position, couleur, type)
+================================================================================
+*/
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Light
+{
+protected:
+	// contient la position de la source de lumière
+	glm::vec3 _lightPosition;
+	// contient la couleur de la source de lumière
+	glm::vec3 _lightColor;
+	// TODO type
+public:
+	// constructeur vide
+	Light();
+	// constructeur avec paramètres
+	Light(glm::vec3& lightPosition, glm::vec3& lightColor);
+	// ajoute ou remplace la position de la source de lumière
+	void addLightPosition(glm::vec3& lightPosition);
+	// ajoute ou remplace la couleur de la source de lumière
+	void addLightColor(glm::vec3& lightColor);
+	// retourne la position de la source de lumière
+	glm::vec3 getLightPosition();
+	// retourne la couleur de la source de lumière
+	glm::vec3 getLightColor();
+};
+
+#endif
